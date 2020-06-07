17eaf551f9fc04326ab75c64bee93cf29323db53
JeanLinux <jeanc.aldea1@gmail.com>
2020-06-07 02:57:07 -0400

    Add animation with library The library is  rnosov/react-reveal

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import "./App.css";
 import AboutUs from "./components/molecules/AboutUs";
 import MainSection from "./components/molecules/MainSection";
 import Another from "./components/molecules/Another";
+import Zoom from "react-reveal/Zoom";
 // import WspButton from "./components/atoms/WspButton";
 
 function App() {
   return (
     <>
       <MainSection />
-      <AboutUs />
-      <Another />
+      <Zoom>
+        <AboutUs />
+        <Another />
+      </Zoom>
     </>
   );
 }
