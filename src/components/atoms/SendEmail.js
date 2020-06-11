26c87bc570b5595ad544a5ef82f8dd7e592d98cb
JeanLinux <jeanc.aldea1@gmail.com>
2020-06-11 17:02:37 -0400

    Refactorized emailJS and ContactUS Se separa la logica del envio con emailjs de contactUS y este se convierte a clase para poder ocupar los estados del formulario

---
+++ src/components/atoms/SendEmail.js
@@ -0,0 +1,29 @@
+import emailjs from "emailjs-com";
+import swal from "sweetalert";
+
+export default function sendEmail(e) {
+  e.preventDefault();
+  console.log(e);
+  emailjs
+    .sendForm("gmailj", "test1", e.target, "user_zWOk6AlMRHvU5HEskUp4D")
+    .then(
+      (result) => {
+        console.log(result.text);
+        if (result.text === "OK") {
+          swal(
+            "Email enviado!",
+            "Nos contactaremos contigo a la brevedad!",
+            "success"
+          );
+          limpiar();
+        }
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+}
+
+function limpiar() {
+  document.getElementById("create-course-form").reset();
+}
--- src/components/molecules/ContacUs.js
+++ src/components/molecules/ContacUs.js
@@ -1,77 +1,51 @@
 import React from "react";
-import emailjs from "emailjs-com";
 import "../../sass/Contact.scss";
 import Zoom from "react-reveal/Zoom";
-import swal from "sweetalert";
+import SendEmail from "../atoms/SendEmail";
 
-export default function ContactUs() {
-  function sendEmail(e) {
-    e.preventDefault();
-
-    emailjs
-      .sendForm("gmailj", "test1", e.target, "user_zWOk6AlMRHvU5HEskUp4D")
-      .then(
-        (result) => {
-          console.log(result.text);
-          if (result.text === "OK") {
-            swal(
-              "Email enviado!",
-              "Nos contactaremos contigo a la brevedad!",
-              "success"
-            );
-            limpiar();
-          }
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+export default class ContactUs extends React.Component {
+  render() {
+    return (
+      <div className="big_container">
+        <div className="big_container__title">Comunicate con nosotros!</div>
+        <Zoom>
+          <form
+            className="contact_form"
+            onSubmit={SendEmail}
+            id="create-course-form"
+          >
+            <div className="contact_form__information">
+              <div className="contact__name">
+                <div className="labell">
+                  <label>Nombre</label>
+                </div>
+                <input className="inputc" type="text" name="user_name" />
+              </div>
+              <div className="contact_form__number">
+                <div className="labell">
+                  <label>Numero</label>
+                </div>
+                <input className="inputc2" type="text" name="contact_number" />
+              </div>
+              <div className="contact__email">
+                <div className="labell">
+                  <label>Email de contacto</label>
+                </div>
+                <input className="inputc" type="email" name="user_email" />
+              </div>
+              <div className="contact__message">
+                <div className="labell">
+                  <label>Mensaje breve sobre tu duda</label>
+                </div>
+                <textarea name="message" className="text_area" />
+              </div>
+              <div>
+                <input type="submit" value="Send" />
+              </div>
+            </div>
+          </form>
+        </Zoom>
+      </div>
+    );
   }
-
-  return (
-    <div className="big_container">
-      <div className="big_container__title">Comunicate con nosotros!</div>
-      <Zoom>
-        <form
-          className="contact_form"
-          onSubmit={sendEmail}
-          id="create-course-form"
-        >
-          <div className="contact_form__information">
-            <div className="contact__name">
-              <div className="labell">
-                <label>Nombre</label>
-              </div>
-              <input className="inputc" type="text" name="user_name" />
-            </div>
-            <div className="contact_form__number">
-              <div className="labell">
-                <label>Numero</label>
-              </div>
-              <input className="inputc2" type="text" name="contact_number" />
-            </div>
-            <div className="contact__email">
-              <div className="labell">
-                <label>Email de contacto</label>
-              </div>
-              <input className="inputc" type="email" name="user_email" />
-            </div>
-            <div className="contact__message">
-              <div className="labell">
-                <label>Mensaje breve sobre tu duda</label>
-              </div>
-              <textarea name="message" className="text_area" />
-            </div>
-            <div>
-              <input type="submit" value="Send" />
-            </div>
-          </div>
-        </form>
-      </Zoom>
-    </div>
-  );
-}
-
-function limpiar() {
-  document.getElementById("create-course-form").reset();
 }
